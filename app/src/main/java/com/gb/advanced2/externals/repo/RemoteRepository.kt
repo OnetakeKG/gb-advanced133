78e93d331041229e0281bc546d3bc38f6431fa19
OnetakeKG <86379850+OnetakeKG@users.noreply.github.com>
2022-03-31 18:41:14 +0300

    31 03

--- app/src/main/java/com/gb/advanced2/externals/repo/RemoteRepository.kt
+++ app/src/main/java/com/gb/advanced2/externals/repo/RemoteRepository.kt
@@ -1,6 +1,5 @@
 package com.gb.advanced2.externals.repo
 
-import android.util.Log
 import com.gb.advanced2.app.Contract
 import com.gb.advanced2.entities.Article
 import com.gb.advanced2.entities.Articles
@@ -10,12 +9,9 @@ import io.reactivex.Observable
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
 
 class RemoteRepository : Contract.Model {
     private val service: ApiService = makeRetrofit()
-
     private fun makeRetrofit(): ApiService {
         val retrofit = Retrofit.Builder()
             .baseUrl(ApiService.BASE_URL)
@@ -25,9 +21,7 @@ class RemoteRepository : Contract.Model {
             .build()
         return retrofit.create(ApiService::class.java)
     }
-
     override fun getArticles(searchString: String): Observable<Articles> =
-        // конвертация retrofit объектов в entities.Article
         service.search(searchString).map { entries ->
             val out = Articles()
             for (entry in entries) {
@@ -36,16 +30,11 @@ class RemoteRepository : Contract.Model {
                 val sb = StringBuilder()
                 for (meaning in entry.meanings) {
                     val trans = meaning.translation ?: continue
-
-                    // разделение нескольких смыслов через ;
                     if (sb.isNotEmpty()) {
                         sb.append("; ")
                     }
-
-                    // основной текст
                     trans.text?.let { sb.append(it) }
 
-                    // пояснительный текст
                     if (trans.note != null && trans.note != "") {
                         sb.append(" (${trans.note})")
                     }
